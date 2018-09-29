546935925789bee1fd1175051c37aa58b5549260
sirivat <dsirivat17@gmail.com>
2018-09-29 16:59:01 -0700

    Configure webpack

---
+++ index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style/main.css";
+import App from "./frontend/app.jsx";
+
+ReactDOM.render(<App />, document.getElementById("root"));
