36c4bdfaea7ded3a33f2bd451fccf6bdd3c6187d
sirivat <dsirivat17@gmail.com>
2018-09-29 18:13:11 -0700

    Integrate css

--- index.jsx
+++ index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style/main.css";
-import App from "./frontend/app.jsx";
+
+import App from "./frontend/app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
